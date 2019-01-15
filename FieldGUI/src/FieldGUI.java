ce245109859ed734a8949e5cf906aeb80644bf24
coolpeepz <colin.r.schultz@gmail.com>
2019-01-15 10:18:32 -0800

    changed message protocol

--- FieldGUI/src/FieldGUI.java
+++ FieldGUI/src/FieldGUI.java
@@ -98,7 +98,7 @@ public class FieldGUI extends Application {
                 //rectangle.setY(e.getY() + 125);
                 
             }
-            client.sendData(((x - 0.28) * 4.2672/0.44) + " " + (y * 4.2672));
+            client.sendData("move " + ((x - 0.28) * 4.2672/0.44) + " " + (y * 4.2672));
             System.out.print(coord + "\n");
             label.setText(coord);
         });
